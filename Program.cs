68f90560efb409634a26aa9b71111137044b8af1
Türker Turhan <86050476+t2rker@users.noreply.github.com>
2021-09-19 22:14:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BankaMatik_uygulaması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mevcutBakiye = 0, islem;
+
+            islem:
+            Console.WriteLine("---------------------------------------------------------------------");
+            Console.WriteLine("Viable Bank'a hoşgeldiniz. Lütfen yapacağınız işlemi seçiniz.");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("Para yatırmak için 1'e basınız.");
+            Console.WriteLine("Para çekmek için 2'e basınız.");
+            Console.WriteLine("Mevcut bakiyenizi görmek için 3'e basınız.");
+            islem = Convert.ToInt32(Console.ReadLine());
+            
+
+
+            switch(islem)
+            {
+                case 1:
+                    Console.WriteLine("Lütfen yatıracağınız para miktarını seçiniz.");
+                    int yatırılacakMiktar = Convert.ToInt32(Console.ReadLine());
+                    mevcutBakiye = yatırılacakMiktar + mevcutBakiye;
+                    Console.WriteLine("İşlem başarılı. Yeni bakiyeniz: " + (mevcutBakiye));
+                    Console.WriteLine("---------------------------------------------------------------------");
+                    System.Threading.Thread.Sleep(2000);
+
+                    break;
+
+                case 2:
+                    Console.WriteLine("Lütfen yatıracağınız para miktarını seçiniz.");
+                    int cekilecekMiktar = Convert.ToInt32(Console.ReadLine());
+                    mevcutBakiye = cekilecekMiktar - mevcutBakiye;
+                    Console.WriteLine("İşlem başarılı. Yeni bakiyeniz: " + (mevcutBakiye));
+                    Console.WriteLine("---------------------------------------------------------------------");
+                    System.Threading.Thread.Sleep(2000);
+                    break;
+
+                case 3:
+                    Console.WriteLine("Mevcut bakiyeniz: " + (mevcutBakiye));
+                    Console.WriteLine("---------------------------------------------------------------------");
+                    System.Threading.Thread.Sleep(2000);
+                    break;
+            }
+
+            goto islem;
+        }
+
+    }
+}
